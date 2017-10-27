d3b70bcfd7444324444fd34e8e7d6b4bd0a2cfd4
ivalexey <megacritic@gmail.com>
2017-10-27 15:45:36 +0300

    Done CreateInOther

--- dsl/procedures/CreateInOther/steps/step1.pl
+++ dsl/procedures/CreateInOther/steps/step1.pl
@@ -8,16 +8,16 @@ my $soapMethodName = 'Create';
 
 # List of the names of optional paramters
 my @optionalParams = (
-    'CSYSDEFModel',
-    'MonSpecInherit',
-    'RTASpecInherit',
-    'WLMSpecInherit',
-    'LNKSWSCGParm',
+  #  'CSYSDEFModel',
+  #  'MonSpecInherit',
+  #  'RTASpecInherit',
+  #  'WLMSpecInherit',
+  #  'LNKSWSCGParm',
 
 );
 # TODO Get rid of this -- we shouldn't need it, we already have a list of all parameters and a list of which are optional
 my @mandatoryParams = (
-    'LocationName',
+  #  'LocationName',
     'ObjectData',
 
 );
@@ -35,20 +35,19 @@ for my $p (@optionalParams, @mandatoryParams) {
     }
 }
 
-my $data =
-SOAP::Data->name('CCV530' => \SOAP::Data->value(
+my @data =
+SOAP::Data->name($soapMethodName => \SOAP::Data->value(
     SOAP::Data->name('LocationCriteria' => \SOAP::Data->value(
-        SOAP::Data->name('LocationName' => $params{'LocationName'}),
-        SOAP::Data->name('LocationType' => $params{'LocationType'})
-    )) ,
+        SoapData('LocationName'),
+        SoapData('LocationType')
+    )),
     SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
-        SOAP::Data->name('ObjName' => $params{'ObjName'}),
-        SOAP::Data->name('ObjGroup' => $params{'ObjGroup'}),
-        SOAP::Data->name('ObjType' => $params{'ObjType'}),
-  )) ,
+        SoapData('ObjName'),
+        SoapData('ObjGroup'),
+        SoapData('ObjType'),
+  )),
   SOAP::Data->name('InputData' => \SOAP::Data->value(
-        @paramsForRequest
-        #  SoapData('ObjectData') //TODO decide how to get specific values for necessary ObjectData.
+         @paramsForRequest
     ))
 ));
 
