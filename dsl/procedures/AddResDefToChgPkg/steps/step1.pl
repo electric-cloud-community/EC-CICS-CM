6cc2b8f2b2a04abe780584b4e0e94485f37f7e97
RDearnaley <rdearnaley@electric-cloud.com>
2017-10-30 22:43:19 -0700

    Fix merge issues

--- dsl/procedures/AddResDefToChgPkg/steps/step1.pl
+++ dsl/procedures/AddResDefToChgPkg/steps/step1.pl
@@ -9,10 +9,6 @@ my $soapMethodName = 'Add';
 # List of the names of optional paramters
 my @optionalParams = ('ObjectCriteria');
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ab0b686652ee6e3b8d81e4f784ff46eed057ef76
 $[/myPlugin/project/ec_perl_metadata]
 
 $[/myPlugin/project/ec_perl_code_block_1]
@@ -20,7 +16,6 @@ $[/myPlugin/project/ec_perl_code_block_1]
 # Procedure-specific Code
 # -----------------------
 
-<<<<<<< HEAD
 # Build @ObjectCriteria
 my @ObjectCriteria;
 if ($params{'ObjectCriteria'}.length == 0) {
@@ -50,27 +45,13 @@ if ($params{'ObjectCriteria'}.length == 0) {
         SOAP::Data->type('xml' => $objectCriteria)
     ));    
 }
-=======
-my @validCriteriaKeys = ('ObjGroup', 'ObjType', 'ObjName');
-
-my @result = makeAddObjectCriteria($params{'ObjectCriteria'}, 'KeyA', @validCriteriaKeys);
->>>>>>> ab0b686652ee6e3b8d81e4f784ff46eed057ef76
 
 my @data  =
 SOAP::Data->name($soapMethodName => \SOAP::Data->value(
     SOAP::Data->name('LocationCriteria' => \SOAP::Data->value(
             SoapData('LocationType')
     )),
-<<<<<<< HEAD
     SOAP::Data->name('ObjectCriteria' => @ObjectCriteria),
-=======
-    SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
-        SoapData('CConfig'),
-        SOAP::Data->value(
-            @result
-        )
-    )),
->>>>>>> ab0b686652ee6e3b8d81e4f784ff46eed057ef76
     SOAP::Data->name('InputData' => \SOAP::Data->value(
         SoapData('ContainerName'),
         SoapData('ContainerType')
