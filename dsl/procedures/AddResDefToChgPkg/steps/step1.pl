32c5bcb4fa0a0d12ec9a047fb5c17342212805a6
RDearnaley <rdearnaley@electric-cloud.com>
2017-10-30 22:27:47 -0700

    Changes to AddResDefToChgPkg
    Also some fixes and improvements to the shared code

--- dsl/procedures/AddResDefToChgPkg/steps/step1.pl
+++ dsl/procedures/AddResDefToChgPkg/steps/step1.pl
@@ -9,8 +9,6 @@ my $soapMethodName = 'Add';
 # List of the names of optional paramters
 my @optionalParams = ('ObjectCriteria');
 
-# TODO Add more procedure-specific metadata: how to structure XML, etc ?
-
 $[/myPlugin/project/ec_perl_metadata]
 
 $[/myPlugin/project/ec_perl_code_block_1]
@@ -18,24 +16,42 @@ $[/myPlugin/project/ec_perl_code_block_1]
 # Procedure-specific Code
 # -----------------------
 
-# TODO Can we figure out a way to drive this with metadata so it could be shared?
+# Build @ObjectCriteria
+my @ObjectCriteria;
+if ($params{'ObjectCriteria'}.length == 0) {
+    
+    # No ObjectCriteria, so we only have one element, and can ommit the <ListCount> and <ListElement>
+    @ObjectCriteria = SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
+        SOAP::Data->name('KeyA' => \SOAP::Data->value(
+            SoapData('ObjGroup'),
+            SoapData('ObjType'),
+            SoapData('ObjName'),
+        ))
+    ));
+} else {
+
+    # Combine ObjName, ObjGroup, ObjType, and ObjectCriteria into @ObjectCriteria
+    my $objectCriteria = $params{'ObjectCriteria'};
+    #### TODO Confirm $objectCriteria is a valid XML fragment matching the expected schema for this method
+    my @matches = $objectCriteria =~ m/<ObjectData>/si;
+    my $listCount = 1 + @matches;
+    @ObjectCriteria = SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
+        SOAP::Data->name('ListCount' => $listCount),
+        SOAP::Data->name('KeyA' => \SOAP::Data->value(
+            SoapData('ObjGroup'),
+            SoapData('ObjType'),
+            SoapData('ObjName'),
+        )),
+        SOAP::Data->type('xml' => $objectCriteria)
+    ));    
+}
 
 my @data  =
 SOAP::Data->name($soapMethodName => \SOAP::Data->value(
     SOAP::Data->name('LocationCriteria' => \SOAP::Data->value(
             SoapData('LocationType')
     )),
-    SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
-        SoapData('CConfig'),
-#        SOAP::Data->name('ListCount' => 1), # TODO Handle ObjectCriteria non-empty case
-#        SOAP::Data->name('ListElement' => \SOAP::Data->value(
-            SOAP::Data->name('keya' => \SOAP::Data->value(
-                SoapData('ObjName'),
-                SoapData('ObjType'),
-                SoapData('ObjDefVer'),
-            )),
-#        )),
-    )),
+    SOAP::Data->name('ObjectCriteria' => @ObjectCriteria),
     SOAP::Data->name('InputData' => \SOAP::Data->value(
         SoapData('ContainerName'),
         SoapData('ContainerType')
