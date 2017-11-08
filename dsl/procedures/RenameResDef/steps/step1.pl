f5dedf5b25fe1128d2b50c7b93420ea2b409b877
ivalexey <megacritic@gmail.com>
2017-11-08 19:51:50 +0200

    Implemented handling ObjectData in RenameResDef with key,value

--- dsl/procedures/RenameResDef/steps/step1.pl
+++ dsl/procedures/RenameResDef/steps/step1.pl
@@ -23,19 +23,28 @@ $[/myPlugin/project/ec_perl_code_block_1]
 my @mParams = ('ObjName', 'ObjGroup', 'ObjType');
 my @ObjectCriteria = createObjectCriteria(\@mParams, 0, "", %params);
 
-# Combine ObjName, ObjGroup, ObjType, and ObjectCriteria into @ObjectCriteria
-my $inputData = $params{'ObjectData'};
-my @matchesTarget = $inputData =~ m/<TargetElement>/si;
-my $targetCount = 1 + @matchesTarget;
-my @InputData = SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
-        SOAP::Data->name('TargetCount' => $targetCount),
-        SOAP::Data->name('TargetElement' => \SOAP::Data->value(
-                SoapData('TargetName'),
-                SoapData('TargetGroup')
-            )),
-        SOAP::Data->type('xml' => $inputData)
-    ));
-
+my @ObjectData;
+if (length($params{'ObjectData'}) == 0) {
+    print "ERROR: Input Data Name-Value Pairs missing!";
+    exit -1;
+} else {
+    # Build @ObjectData
+    my @parts = split(/\s+/s, $params{'ObjectData'}); # Split at whitesapece (including line breaks)
+    push(@ObjectData, SOAP::Data->name('TargetCount' => scalar(@parts)));
+    foreach my $part (@parts) {
+        my @pieces = split(/,/, $part, 2); # Split at first ',' into name and value
+        if (@pieces == 2) {
+            my @targetElement = SOAP::Data->name('TargetElement' => \SOAP::Data->value(
+                    SOAP::Data->name('TargetName' => $pieces[0]),
+                    SOAP::Data->name('TargetGroup' => $pieces[1]),
+            ));
+            push(@ObjectData, @targetElement);
+        } else {
+            print "ERROR: Unable to parse Object Data Name-Value Pairs near '$part'!";
+            exit -1;
+        }
+    }
+}
 
 my @data =
 SOAP::Data->name($soapMethodName => \SOAP::Data->value(
@@ -47,7 +56,7 @@ SOAP::Data->name($soapMethodName => \SOAP::Data->value(
         SOAP::Data->name('ObjectCriteria' => @ObjectCriteria),
     )),
     SOAP::Data->name('InputData' => \SOAP::Data->value(
-        SOAP::Data->name('InputData' => @InputData),
+        @ObjectData
     )),
     SOAP::Data->name('ProcessParms' => \SOAP::Data->value(
         SoapData('Replace')
