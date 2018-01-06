1174e586d97784aacebd6aa2a2dc1d20dde08b2c
RDearnaley <rdearnaley@electric-cloud.com>
2018-01-05 20:22:10 -0800

    Assorted minor fixes and TODOs

--- dsl/procedures/Import/steps/step1.pl
+++ dsl/procedures/Import/steps/step1.pl
@@ -21,17 +21,43 @@ $[/myPlugin/project/ec_perl_metadata]
 
 $[/myPlugin/project/ec_perl_code_block_1]
 
-# Validateion
+# Validation
 
 if(($params{'resDefinition'} eq 'SelectionCriteria')) {
-    if(!length($params{'CPID'})) {
-        print "ERROR: 'CPID' hould be specified when 'Select resource definitions' is set to 'Selection Criteria'.";
+    # Validate optional parameters
+    if(!(length($params{'CPID'}) > 0)) {
+        print "ERROR: 'CPID' should be specified when 'Select resource definitions' is set to 'Selection Criteria'.";
+        exit -1;
+    }    
+    if((length($params{'ObjName'}) > 0) || (length($params{'ObjType'}) > 0) || (length($params{'ObjGroup'}) > 0)) {
+        print "ERROR: 'Resource Name', 'Resource Type', and 'Resource Group' must not be specified when 'Select resource definitions' is set to 'Selection Criteria'.";
         exit -1;
     }
 }
-elsif(!length($params{'ObjName'}) || !length($params{'ObjType'}) || !length($params{'ObjGroup'})) {
-    print "ERROR: 'Resource Name', 'Resource Type' and 'Resource Group' should be specified when 'Select resource definitions' is set to 'Object Criteria'.";
-    exit -1;
+else {
+    # Validate optional parameters
+    if(!(length($params{'ObjName'}) > 0) || !(length($params{'ObjType'}) > 0)) {
+        print "ERROR: 'Resource Name' and 'Resource Type' should be specified when 'Select resource definitions' is set to 'Object Criteria'.";
+        exit -1;
+    }
+    if((length($params{'CPID'}) > 0) || (length($params{'EventID'}) > 0)) {
+        print "ERROR: 'CPID' and 'EventID' must not be specified when 'Select resource definitions' is set to 'Object Criteria'.";
+        exit -1;
+    }
+    
+    # Validate Object Group against Object Type
+    if (($params{'ObjType'} eq 'RESGROUP') || ($params{'ObjType'} eq 'RESDESC')) {
+        if (length($params{'ObjGroup'}) > 0) {
+            print "ERROR: You cannot specify an Resource Group when the Resource Type is 'ResGroup (Group for CSD)' or 'ResDesc (List for CSD)'!\n";
+            exit -1;
+        }
+    }
+    
+    # Validate wildcards are at end
+    if (($params{'ObjName'} =~ /\*.+$/) || ($params{'ObjGroup'} =~ /\*.+$/)) {
+        print "ERROR: The wildcard character '*' must only occur at the end of the Resource Name or Resource Group!\n";
+        exit -1;
+    }
 }
 
 my @criteria;
@@ -45,7 +71,7 @@ else {
     @criteria = SOAP::Data->name('ObjectCriteria' => \SOAP::Data->value(
         SoapData('ObjName'),
         SoapData('ObjType'),
-        SoapData('ObjGroup')
+        SoapDataOptional('ObjGroup')
     ));
 }
 
@@ -64,8 +90,8 @@ my @data =
             SoapData('TargetLocationName')
         )),
         SOAP::Data->name('ProcessParms' => \SOAP::Data->value(
-            SoapData('RegisterCPID'),
-            SoapData('PurgeImportedRecords')
+            SoapDataOptional('RegisterCPID'),
+            SoapDataOptional('PurgeImportedRecords')
         ))
     ));
 
